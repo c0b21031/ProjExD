519955fbe04be016296ce7cf497ac327ec06834d
ohashi <c0b2103135@edu.teu.ac.jp>
2022-10-25 17:24:27 +0900

    逃げろこうかとん

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -12,6 +12,14 @@ def check_bound(obj_rct,scr_rct):
         tate=-1
     return yoko,tate
 
+#bomb2を次第に大きくする関数()
+#def change_large(change_y):
+#    if change_y <0.40:
+#        change_y += 0.01
+#    else:
+#        change_y=0.01
+#    return change_y 
+
 def main():
     #ウィンドウ生成
     pg.display.set_caption("逃げろ!こうかとん")
@@ -28,13 +36,24 @@ def main():
     tori_rct=tori_sfc.get_rect()
     tori_rct.center=900,400
 
-    #空のSurfaceの生成
+
+    #爆弾のSurfaceの生成
     bomb_sfc = pg.Surface((20, 20)) # 空のSurface
     bomb_sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
     pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10) # 円を描く
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx = randint(0, scrn_rct.width)
     bomb_rct.centery = randint(0, scrn_rct.height)    
+
+    
+    
+    #爆弾2のSurfaceの生成
+    change_y=0.01
+    bomb2_sfc=pg.image.load("C:/Users/admin/Documents/ProjExD2022/ex04/enemy.png")
+    bomb2_sfc=pg.transform.rotozoom(bomb2_sfc,0,0.20)
+    bomb2_rct=tori_sfc.get_rect()
+    bomb2_rct.centerx = randint(0, scrn_rct.width)
+    bomb2_rct.centery = randint(0, scrn_rct.height)
 
     vx,vy=+1,+1
     #背景画像のbilt
@@ -66,16 +85,22 @@ def main():
         scrn_sfc.blit(tori_sfc, tori_rct)
 
         yoko,tate=check_bound(bomb_rct, scrn_rct)
+        yoko,tate=check_bound(bomb2_rct,scrn_rct)
         vx *= yoko
         vy *= tate
         bomb_rct.move_ip(vx,vy)
+        bomb2_rct.move_ip(vx,vy)
         scrn_sfc.blit(bomb_sfc,bomb_rct)
+        scrn_sfc.blit(bomb2_sfc,bomb2_rct)
 
-        if tori_rct.colliderect(bomb_rct): # こうかとんrctが爆弾rctと重なったら
+        
+
+        if tori_rct.colliderect(bomb_rct) or tori_rct.colliderect(bomb2_rct): # こうかとんrctが爆弾1rctか爆弾2rctと重なったら
             return
 
         pg.display.update()
         clock.tick(1000)
+
 
 
 if __name__=="__main__":
